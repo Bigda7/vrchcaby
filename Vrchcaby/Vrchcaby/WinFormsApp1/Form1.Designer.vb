3517915449769d36a30d8dc6466a52d9f7c8356c
Bigda7 <bodik.mozgovoy@gmail.com>
2024-11-03 18:52:42 +0100

    Add files via upload

---
+++ Vrchcaby/Vrchcaby/WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,190 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Label3 = New Label()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Button4 = New Button()
+        Label7 = New Label()
+        Label8 = New Label()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(150, 204)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 0
+        Button1.Text = "Player 1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(597, 204)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 1
+        Button2.Text = "Player 2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(376, 273)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(75, 23)
+        Button3.TabIndex = 2
+        Button3.Text = "Run"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point, CByte(204))
+        Label3.Location = New Point(78, 109)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(56, 65)
+        Label3.TabIndex = 5
+        Label3.Text = "0"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point, CByte(204))
+        Label1.Location = New Point(169, 109)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(56, 65)
+        Label1.TabIndex = 12
+        Label1.Text = "0"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point, CByte(204))
+        Label2.Location = New Point(262, 109)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(56, 65)
+        Label2.TabIndex = 13
+        Label2.Text = "0"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point, CByte(204))
+        Label4.Location = New Point(523, 109)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(56, 65)
+        Label4.TabIndex = 14
+        Label4.Text = "0"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point, CByte(204))
+        Label5.Location = New Point(616, 109)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(56, 65)
+        Label5.TabIndex = 15
+        Label5.Text = "0"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point, CByte(204))
+        Label6.Location = New Point(716, 109)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(56, 65)
+        Label6.TabIndex = 16
+        Label6.Text = "0"
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(376, 344)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(75, 23)
+        Button4.TabIndex = 19
+        Button4.Text = "Clear"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(169, 273)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(13, 15)
+        Label7.TabIndex = 20
+        Label7.Text = "0"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(616, 277)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(13, 15)
+        Label8.TabIndex = 21
+        Label8.Text = "0"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label8)
+        Controls.Add(Label7)
+        Controls.Add(Button4)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(Label3)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+
+End Class
---
+++ Vrchcaby/Vrchcaby/WinFormsApp1/Form1.vb
@@ -0,0 +1,86 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Randomize()
+        Label1.Text = Int(Rnd() * 6) + 1
+        Label2.Text = Int(Rnd() * 6) + 1
+        Label3.Text = Int(Rnd() * 6) + 1
+        Label7.Text = Val(Label1.Text) + Val(Label2.Text) + Val(Label3.Text)
+        Button1.Enabled = False
+        CheckButton3Availability()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Randomize()
+        Label4.Text = Int(Rnd() * 6) + 1
+        Label5.Text = Int(Rnd() * 6) + 1
+        Label6.Text = Int(Rnd() * 6) + 1
+        Label8.Text = Val(Label4.Text) + Val(Label5.Text) + Val(Label6.Text)
+        Button2.Enabled = False
+        CheckButton3Availability()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If Val(Label7.Text) > Val(Label8.Text) Then
+            Dim result1 As DialogResult = MessageBox.Show("Player 1 won!", "Results", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            If result1 = DialogResult.OK Then
+                Result()
+                CheckButton3Availability()
+            End If
+        ElseIf Val(Label8.Text) > Val(Label7.Text) Then
+            Dim result2 As DialogResult = MessageBox.Show("Player 2 won!", "Results", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            If result2 = DialogResult.OK Then
+                Result()
+                CheckButton3Availability()
+            End If
+        ElseIf Val(Label7.Text) = Val(Label8.Text) And Val(Label7.Text) <> 0 And Val(Label7.Text) <> 0 Then
+            Dim draw As DialogResult = MessageBox.Show("Draw!", "Results", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            If draw = DialogResult.OK Then
+                Result()
+                CheckButton3Availability()
+            End If
+        End If
+
+        Button1.Enabled = True
+        Button2.Enabled = True
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If Label7.Text = "0" AndAlso Label8.Text = "0" Then
+            MessageBox.Show("Error!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+        Result()
+        CheckButton3Availability()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Button3.Enabled = False
+    End Sub
+    Private Sub Result()
+        Label1.Text = "0"
+        Label2.Text = "0"
+        Label3.Text = "0"
+        Label4.Text = "0"
+        Label5.Text = "0"
+        Label6.Text = "0"
+        Label7.Text = "0"
+        Label8.Text = "0"
+        Button1.Enabled = True
+        Button2.Enabled = True
+
+    End Sub
+
+    Private Sub Label7_Click(sender As Object, e As EventArgs) Handles Label7.Click
+
+    End Sub
+
+    Private Sub Label8_Click(sender As Object, e As EventArgs) Handles Label8.Click
+
+    End Sub
+    Private Sub CheckButton3Availability()
+        If Val(Label7.Text) <> 0 And Val(Label8.Text) <> 0 Then
+            Button3.Enabled = True
+        Else
+            Button3.Enabled = False
+        End If
+    End Sub
+End Class
